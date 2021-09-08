32e6a19ea4c199b1e5c320f312327d0a820013c3
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-09-08 18:49:40 +0200

    background colour added, title text added, new font added to stack

--- TyroneCard/ContentView.swift
+++ TyroneCard/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.00, green: 0.19, blue: 0.31)
+                .edgesIgnoringSafeArea(.all)
+            Text("Tyrone Oggen")
+                .font(.title)
+                .foregroundColor(Color.white)
+                .bold()
+                .padding()
+        }
     }
 }
 
